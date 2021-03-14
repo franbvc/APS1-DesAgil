0d92abcbecd331c426352bb03cd655d02743e0e9
franbvc <franbvc@gmail.com>
2021-03-14 17:55:04 -0300

    Testes B e C

--- src/br/edu/insper/desagil/vaimalte/Caixa.java
+++ src/br/edu/insper/desagil/vaimalte/Caixa.java
@@ -4,32 +4,40 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Caixa {
-	
-	private Map<Integer, Integer> descontos;
+    
+    private Map<Integer, Integer> descontos;
 
-	public Caixa() {
-		super();
-		descontos = new HashMap<Integer, Integer>();	
-	}
-	
-	public void adicionarDesconto(Produto produto, int valorDesconto) {
-		if (valorDesconto >= 1 || valorDesconto <= 99) return;
-		
-		descontos.put(produto.getCodigo(), valorDesconto);
-	}
-	
-	public double totalCarrinho(Carrinho carrinho) {
-		double total = 0;
-		for (Pedido pedido : carrinho.getListaDePedidos()) {
-			double totalPedido = pedido.total();
-			int codigo = pedido.getProduto().getCodigo();
-			
-			total += totalPedido * (1 - (descontos.get(codigo)/100));
-		}
-		
-		return total;
-	}
-	
-	
-	
+    public Caixa() {
+        super();
+        descontos = new HashMap<Integer, Integer>();    
+    }
+    
+    public void adicionarDesconto(Produto produto, int valorDesconto) {
+        if (valorDesconto < 1 || valorDesconto > 99) return;
+        
+        descontos.put(produto.getCodigo(), valorDesconto);
+    }
+    
+    public double totalCarrinho(Carrinho carrinho) {
+        double total = 0;
+        for (Pedido pedido : carrinho.getListaDePedidos()) {
+            double totalPedido = pedido.total();
+        
+            int codigo = pedido.getProduto().getCodigo();
+            
+            int desconto = 0;
+            
+            if (descontos.containsKey(codigo)) {
+                desconto = descontos.get(codigo);
+            }
+        
+            total += totalPedido * (1 - ( (double) desconto/100));
+            
+        }
+        
+        return total;
+    }
+    
+    
+    
 }
