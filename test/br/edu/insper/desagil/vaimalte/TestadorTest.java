ecbbd235ebddd9210c59e669bd50d27a3d5a9d7c
franbvc <franbvc@gmail.com>
2021-03-14 15:39:32 -0300

    Cria esqueleto 2

---
+++ test/br/edu/insper/desagil/vaimalte/TestadorTest.java
@@ -0,0 +1,39 @@
+package br.edu.insper.desagil.vaimalte;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestadorTest {
+	private Testador t;
+
+	@BeforeEach
+	private void setUp() {
+	    t = new Testador();
+	}
+
+	@Test
+	public void testeA() {
+	    assertTrue(t.testeA());
+	}
+
+	@Test
+	public void testeB() {
+	    assertTrue(t.testeB());
+	}
+
+	@Test
+	public void testeC() {
+	    assertTrue(t.testeC());
+	}
+
+	@Test
+	public void testeD() {
+	    assertTrue(t.testeD());
+	}
+
+	@Test
+	public void testeE() {
+	    assertTrue(t.testeE());
+	}
+}
